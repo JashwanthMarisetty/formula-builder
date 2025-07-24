6be8f72b01ee62283c302e37cb092745e888b94e
JashwanthMarisetty <jashwanthmarisetty217@gmail.com>
2025-07-25 01:36:31 +0530

    Add backend models and routes, update package dependencies

---
+++ backend/models/User.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  avatar: {
+    type: String,
+    default:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&dpr=1",
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+  verificationExpires: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+
+  formsCreated: {
+    type: Number,
+    default: 0
+  },
+  totalResponses: {
+    type: Number,
+    default: 0
+  },
+
+}, {
+    timestamps: true,
+});
+
+// Hash password before saving user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// Method to generate a verification token
+userSchema.methods.generateVerificationToken = function () {
+  const token = require("crypto").randomBytes(32).toString("hex");
+  this.verificationToken = token;
+  this.emailVerificationExpires = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
+  return token;
+};
+
+// Method to generate a password reset token
+userSchema.methods.generatePasswordResetToken = function () {
+  const token = require("crypto").randomBytes(32).toString("hex");
+  this.passwordResetToken = token;
+  this.passwordResetExpires = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
